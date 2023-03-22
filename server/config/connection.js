adc80bfe87b6f792f987eb02bf971ab391fc3fd3
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-03-22 09:00:06 -0400

    index. js on seeds

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/travelBuddy', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useCreateIndex: false,
+    // useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
