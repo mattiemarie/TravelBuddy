eb479c4607048485eb8249b8d91373ce4ae71654
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-03-27 19:44:36 -0400

    adding footer.jxs

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../footer/footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>·Group5 · Project 3 · TravelBuddy ·</p>
+    </div>
+  );
+}
+
+export default Footer;
