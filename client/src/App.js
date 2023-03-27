63191adcd4e29b278b06682542ec70393dc629bd
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-03-27 19:53:23 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,53 +6,11 @@ import Results from "./components/results/results";
 import Search from "./components/search/search";
 import Onboarding from './components/onboarding';
 
-
 function App({children}) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
-  
-  };
-
-import "./App.css";
-import axios from "axios";
-import Stripe from "react-stripe-checkout";
-import FaceIcon from "@mui/icons-material/Face";
-import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
-import { Chip, Switch, createTheme, ThemeProvider } from "@material-ui/core";
-import { useState } from "react";
-import Login from "./components/login.js";
-import Signup from "./components/signup.js";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#5ce1e6",
-    },
-  },
-});
-
-function App() {
-  const [checked, setChecked] = useState(false);
-
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
-  };
-
-  const handleToken = async (totalAmount, token) => {
-    try {
-      await axios.post("http://localhost:5000/api/stripe/pay", {
-        token: token.id,
-        amount: totalAmount,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const tokenHandler = (token) => {
-    handleToken(100, token);
   };
 
   return (
